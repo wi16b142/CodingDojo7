99c6bd1b0fc2ae57afc6c686027df2a8f8098712
Sascha Böck <wi16b142@technikum-wien.at>
2017-12-15 20:50:56 +0100

    created mainvm, itemsvm, overviewvm, mytoysvm
    installed mvvmlight
    
    added images

---
+++ CodingDojo7/ViewModel/ItemsVM.cs
@@ -0,0 +1,43 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CodingDojo7.ViewModel
+{
+    public class ItemsVM : ViewModelBase
+    {
+        public ItemsVM(string description, string ageRecc, BitmapImage image)
+        {
+            Description = description;
+            AgeRecc = ageRecc;
+            Image = image;
+        }
+
+        public string Description { get; set; }
+        public string AgeRecc { get; set; }
+        public BitmapImage Image { get; set; }
+        public ObservableCollection<ItemsVM> Items { get; set; }
+
+        public int ItemsCount()
+        {
+            if (Items != null) return Items.Count;
+            else return 0;
+        }
+
+        public void ItemsAdd(ItemsVM newItem)
+        {
+            if(Items == null)
+            {
+                Items = new ObservableCollection<ItemsVM>();
+            }
+
+            Items.Add(newItem);
+        }
+
+    }
+}
