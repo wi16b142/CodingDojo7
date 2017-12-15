a506c678b87b2c43a3cf821fd3e2c774522271e6
Sascha Böck <wi16b142@technikum-wien.at>
2017-12-15 23:07:51 +0100

    edit overviewView, overviewVM, mytoysVM, mytoysView, mainVM, mainView

--- CodingDojo7/ViewModel/MainViewModel.cs
+++ CodingDojo7/ViewModel/MainViewModel.cs
@@ -1,12 +1,34 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Ioc;
 
 namespace CodingDojo7.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        private ViewModelBase currentView;
+        public RelayCommand OverviewBtnClickCmd { get; set; }
+        public RelayCommand MyToysBtnClickCmd { get; set; }
+        public ViewModelBase CurrentView
+        {
+            get { return currentView; ; }
+            set { currentView = value; RaisePropertyChanged(); }
+        }
 
         public MainViewModel()
         {
+            CurrentView = SimpleIoc.Default.GetInstance<OverviewVM>();
+            OverviewBtnClickCmd = new RelayCommand(()=> 
+            {
+                CurrentView = CurrentView = SimpleIoc.Default.GetInstance<OverviewVM>();
+                RaisePropertyChanged();
+            });
+
+            MyToysBtnClickCmd = new RelayCommand(() =>
+            {
+                CurrentView = CurrentView = SimpleIoc.Default.GetInstance<MyToysVM>();
+                RaisePropertyChanged();
+            });
 
         }
     }
--- CodingDojo7/ViewModel/MyToysVM.cs
+++ CodingDojo7/ViewModel/MyToysVM.cs
@@ -4,11 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
 
 namespace CodingDojo7.ViewModel
 {
     public class MyToysVM:ViewModelBase
     {
+        private ObservableCollection<ItemsVM> wishlist;
+        
+        public ObservableCollection<ItemsVM> Wishlist
+        {
+            get { return wishlist; }
+            set { wishlist = value; RaisePropertyChanged(); }
+        }
 
+        public MyToysVM()
+        {
+            Wishlist = new ObservableCollection<ItemsVM>();
+        }
+
+        internal void Add(ItemsVM itemToAdd)
+        {
+            Wishlist.Add(itemToAdd);
+            RaisePropertyChanged();
+        }
     }
 }
